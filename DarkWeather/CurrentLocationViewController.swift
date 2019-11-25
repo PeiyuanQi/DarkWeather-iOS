905c2d1e68ede3e607afd49221bb210ddc7850c2
peiyuanqi <qipeiyuan@outlook.com>
2019-11-25 00:42:02 -0800

    init storyboard`

---
+++ DarkWeather/CurrentLocationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentLocationViewController.swift
+//  DarkWeather
+//
+//  Created by 戚培源 on 2019/11/25.
+//  Copyright © 2019 戚培源. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
